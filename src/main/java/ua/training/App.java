4018637fdfbd8290ef73407ed3ae5160204da76f
Roman Prokopenko <prokopenkor5@gmail.com>
2016-10-28 23:48:50 +0300

    now i harro everybum

--- src/main/java/ua/training/App.java
+++ src/main/java/ua/training/App.java
@@ -6,8 +6,8 @@ package ua.training;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.println( "Harro everybum!" );
     }
 }
