474fbc7be1b44ab04fb20da4a3fac11b2790ebf0
Roman Prokopenko <prokopenkor5@gmail.com>
2016-11-27 21:00:26 +0200

    facade

---
+++ patterns/src/main/java/ua/training/facade/Facade.java
@@ -0,0 +1,63 @@
+package ua.training.facade;
+
+/**
+ * Created by Graffit on 27.11.2016.
+ */
+public class Facade {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Computer computer = new Computer();
+        computer.copy();
+    }
+
+}
+
+// facade
+class Computer {
+    Power power = new Power();
+    DVDRom dvd = new DVDRom();
+    HDD hdd = new HDD();
+
+    void copy() {
+        power.on();
+        dvd.load();
+        hdd.copyFromDVD(dvd);
+    }
+}
+
+class Power {
+    void on() {
+        System.out.println("turn on");
+    }
+
+    void off() {
+        System.out.println("turn off");
+    }
+}
+
+class DVDRom {
+    private boolean data = false;
+
+    public boolean hasData() {
+        return data;
+    }
+
+    public void load() {
+        data = true;
+    }
+
+    public void unload() {
+        data = false;
+    }
+}
+
+class HDD {
+    void copyFromDVD(DVDRom dvdRom) {
+        if (dvdRom.hasData()) {
+            System.out.println("copy data from dvd");
+        } else {
+            System.out.println("put in dvd with data");
+        }
+
+    }
+}
